e1921f152fbe63090638f5396d596695f9d3dc11
Oskar Gustafsson <oskar.gustafsson@inceptron.io>
2025-10-01 16:04:08 +0200

    Alter memcopy striding checks

--- mlir/lib/Conversion/GPUCommon/GPUToLLVMConversion.cpp
+++ mlir/lib/Conversion/GPUCommon/GPUToLLVMConversion.cpp
@@ -1065,8 +1065,10 @@ LogicalResult ConvertMemcpyOpToGpuRuntimeCallPattern::matchAndRewrite(
   auto memRefType = cast<MemRefType>(memcpyOp.getSrc().getType());
 
   if (failed(areAllLLVMTypes(memcpyOp, adaptor.getOperands(), rewriter)) ||
-      !isConvertibleAndHasIdentityMaps(memRefType) ||
       failed(isAsyncWithOneDependency(rewriter, memcpyOp)))
+    return failure();
+
+  if (!(memRefType.getLayout().isIdentity() || memRefType.isStrided()))
     return failure();
 
   auto loc = memcpyOp.getLoc();
